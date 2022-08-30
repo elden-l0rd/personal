48046680dc4117eaf673fd360ccf46c037d9cdd4
elden-l0rd <cfhuangx9@gmail.com>
2022-08-31 03:27:28 +0800

    reupload lab2 & 3 code

---
+++ lab/src/lab3/PlaneSeat.java
@@ -0,0 +1,26 @@
+package lab3;
+import java.util.*;
+public class PlaneSeat {
+	private static int seatId;
+	private boolean assigned;
+	private static int customerId;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		PlaneSeat ob = new PlaneSeat();
+
+		
+		sc.close();
+	}
+	
+	public int getSeatID();
+	
+	public int getCustomerID();
+	
+	public boolean isOccupied();
+	
+	public void assign(int cust_id);
+	
+	public void unAssign();
+
+}
