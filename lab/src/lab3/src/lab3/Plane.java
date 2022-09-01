f6293217af9a6fb0023f7f9d294896a20d8d156b
elden-l0rd <cfhuangx9@gmail.com>
2022-09-01 21:55:54 +0800

    time for test #1

--- lab/src/lab3/src/lab3/Plane.java
+++ lab/src/lab3/src/lab3/Plane.java
@@ -1,20 +1,38 @@
 package lab3;
 
 public class Plane {
-    private static lab3.PlaneSeat[] seat = new PlaneSeat[12];
+    private lab3.PlaneSeat[] seat = new PlaneSeat[12];
     private int numEmptySeat=12;
     
 
     public Plane() {
         for (int i=0; i<12; i++) {
-            seat[i].seatId = i+1;   // initialise seatID number [1,12]
+            this.seat[i].seatId = i+1;   // initialise seatID number [1,12]
         }
     }
 
     private void sortSeats() { // SORT by ascending customerID
         lab3.PlaneSeat[] copy = new PlaneSeat[12];
         copy = seat;
-        // ** sorting algorithm TODO ** //
+        //insertion sort//
+        for (int i=1; i<12; i++) {
+            PlaneSeat k = copy[i];
+            int key = copy[i].getCustomerID();
+            int j = i-1;
+            
+            while (j>=0 && copy[j].getCustomerID()>key) {
+                copy[j+1] = copy[j];
+                j-=1;
+            }
+            copy[j+1] = k;
+        }
+        //then print//
+        for (int i=0; i>12; i++) {
+            if (copy[i].isOccupied()==true) {
+                System.out.println("SeatID "+copy[i].getSeatID()+
+                " assigned to CustomerID "+copy[i].getCustomerID());
+            }
+        }
     }
     
     //methods
@@ -31,8 +49,10 @@ public class Plane {
     }
 
     public void showAssignedSeats(boolean bySeatId) { //if bySeatId=false, by customerId
-        if (bySeatId==false)
+        if (bySeatId==false) {
             sortSeats();
+            return;
+        }
         else {
             for (int i=0; i<12; i++) {
                 if (seat[i].isOccupied()==true) {
@@ -43,10 +63,19 @@ public class Plane {
         }
     }
     public void assignSeat(int seatId, int cust_id) {
-        seat[seatId].assign(cust_id);
+        if (seat[seatId].isOccupied()==true) {
+            System.out.println("Seat Assigned!");
+            return;
+        }
+        else {
+            seat[seatId].assign(cust_id);
+            numEmptySeat--;
+        }
     }
 
     public void unAssignSeat(int seatId) {
         seat[seatId].unAssign();
+        numEmptySeat++;
+        System.out.println("Seat Unassigned!");
     }
 }
--- lab/src/lab3/src/lab3/PlaneApp.java
+++ lab/src/lab3/src/lab3/PlaneApp.java
@@ -5,6 +5,7 @@ public class PlaneApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int choice;
+        Plane Plane = new Plane();
         do {
             System.out.println("(1) Show number of empty seats");
             System.out.println("(2) Show the list of empty seats");
@@ -17,19 +18,35 @@ public class PlaneApp {
             choice = sc.nextInt();
             switch(choice) {
                 case 1:
+                    Plane.showNumEmptySeats();
                     break;
                 case 2:
+                    Plane.showEmptySeats();
                     break;
                 case 3:
+                    System.out.println("The seat assignments are as follows:");
+                    Plane.showAssignedSeats(true);
                     break;
                 case 4:
+                    System.out.println("The seat assignments are as follows:");
+                    Plane.showAssignedSeats(false);
                     break;
                 case 5:
+                    int sid, cid;
+                    System.out.println("Assigning Seat ..");
+                    System.out.println("Please enter SeatID ");
+                    sid = sc.nextInt();
+                    System.out.println("Please enter CustomerID: ");
+                    cid = sc.nextInt();
+                    Plane.assignSeat(sid, cid);
                     break;
                 case 6:
+                    System.out.println("Enter SeatID to unassign customer from: ");
+                    int sd = sc.nextInt();
+                    Plane.unAssignSeat(sd);
                     break;
                 case 7:
-                    break;
+                    return;
             }
         }while(choice<8 && choice>0);
 
