f03c15d145b97ffcfaf6df39ec51f08007c3a28e
elden-l0rd <cfhuangx9@gmail.com>
2022-09-01 19:19:36 +0800

    insert sorting algo + finish PlaneApp

--- lab/src/lab3/src/lab3/Plane.java
+++ lab/src/lab3/src/lab3/Plane.java
@@ -1,20 +1,52 @@
 package lab3;
-import java.util.*;
 
 public class Plane {
-    private int PlaneSeat[] = new PlaneSeat[12];
-    private int numEmptySeat;
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    private static lab3.PlaneSeat[] seat = new PlaneSeat[12];
+    private int numEmptySeat=12;
+    
+
+    public Plane() {
+        for (int i=0; i<12; i++) {
+            seat[i].seatId = i+1;   // initialise seatID number [1,12]
+        }
     }
 
-    public void showNumEmptySeats()
+    private void sortSeats() { // SORT by ascending customerID
+        lab3.PlaneSeat[] copy = new PlaneSeat[12];
+        copy = seat;
+        // ** sorting algorithm TODO ** //
+    }
+    
+    //methods
+    public void showNumEmptySeats() {
+        System.out.println("There are "+numEmptySeat+" seats");
+    }
 
-    public void showEmptySeats()
+    public void showEmptySeats() {
+        for (int i=0; i<12; i++) {
+            if (seat[i].isOccupied()==false) {
+                System.out.println("SeatID "+(i+1));
+            }
+        }
+    }
 
-    public void showAssignedSeats(boolean bySeatId)
+    public void showAssignedSeats(boolean bySeatId) { //if bySeatId=false, by customerId
+        if (bySeatId==false)
+            sortSeats();
+        else {
+            for (int i=0; i<12; i++) {
+                if (seat[i].isOccupied()==true) {
+                    System.out.println("SeatID "+seat[i].getSeatID()+
+                    " assigned to CustomerID "+seat[i].getCustomerID());
+                }
+            }
+        }
+    }
+    public void assignSeat(int seatId, int cust_id) {
+        seat[seatId].assign(cust_id);
+    }
 
-    public void assignSeat(int seatId, int cust_id)
-
-    public void unAssignSeat(int seatId)*/
-} 
+    public void unAssignSeat(int seatId) {
+        seat[seatId].unAssign();
+    }
+}
