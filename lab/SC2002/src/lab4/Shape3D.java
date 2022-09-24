21719ebf3a9b9f414d5050cb19a29815681720a3
elden-l0rd <cfhuangx9@gmail.com>
2022-09-25 03:33:40 +0800

    lab4 wic

---
+++ lab/SC2002/src/lab4/Shape3D.java
@@ -0,0 +1,15 @@
+package lab4;
+import java.util.*;
+
+public class Shape3D {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+
+        
+
+        sc.close();
+    }
+}
