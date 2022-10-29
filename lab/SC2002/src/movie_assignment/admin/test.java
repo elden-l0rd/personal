45be231d41130fadba52822b453f6488298244a9
elden-l0rd <cfhuangx9@gmail.com>
2022-10-29 19:42:05 +0700

    testing

--- lab/SC2002/src/movie_assignment/admin/test.java
+++ lab/SC2002/src/movie_assignment/admin/test.java
@@ -1,12 +1,11 @@
 package movie_assignment.admin;
-import java.util.*;
 
 public class test {
     private static int num =4;
     public static void main(String args[]) {
         double ratings=23.5;
 
-        System.out.printf("%.1f", ratings/num);
+        System.out.printf("%.1f\n", ratings/num);
 
     }
 }
